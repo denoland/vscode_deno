5553b780c7ea4612d4bbd895ccebd57d8cfc74a0
axetroy <axetroy.dev@gmail.com>
2020-02-25 19:43:45 +0800

    test: make sure deno_dir

--- core/deno.test.ts
+++ core/deno.test.ts
@@ -1,3 +1,6 @@
+import execa from "execa";
+import which from "which";
+
 import { getDenoDir, getDenoDepsDir } from "./deno";
 
 test("core / deno", () => {
@@ -5,4 +8,15 @@ test("core / deno", () => {
   expect(getDenoDepsDir()).not.toBe(undefined);
 
   expect(getDenoDepsDir()).toContain(getDenoDir());
+
+  const denoPath = which.sync("deno");
+
+  const ps = execa.sync(denoPath, ["info"]);
+
+  const lines = ps.stdout.split("\n");
+  const firstLine = lines[0];
+
+  const [, denoDir] = /"([^"]+)"/.exec(firstLine) as string[];
+
+  expect(getDenoDir()).toEqual(denoDir);
 });
