a19c04ba2acf0caa124acccdd29c5797c0800755
axetroy <axetroy.dev@gmail.com>
2020-03-04 22:47:19 +0800

    test: add test case

--- core/vscode_settings.test.ts
+++ core/vscode_settings.test.ts
@@ -16,4 +16,19 @@ test("core / vscode_settings", async () => {
     dts_file: ["./demo.d.ts"],
     import_map: undefined
   } as DenoPluginConfig);
+
+  expect(readConfigurationFromVscodeSettings("./file/not/exist")).toEqual(
+    undefined
+  );
+});
+
+test("core / vscode_settings if it empty", async () => {
+  const vscodeDir = path.join(TEST_DIR, "vscode_settings_1");
+  const config = readConfigurationFromVscodeSettings(vscodeDir);
+
+  expect(config).toEqual({
+    enable: false,
+    dts_file: [],
+    import_map: undefined
+  } as DenoPluginConfig);
 });
