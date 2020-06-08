f318281b51e2889ed030d52defa6f7f5f3989821
simwipado <56857885+simwipado@users.noreply.github.com>
2020-06-08 16:16:40 +1000

    Clean up (#58)
    * remove unused files
    * disable deno extension

--- src/language_service.ts
+++
@@ -1,9 +0,0 @@
-// import ts_module from "typescript/lib/tsserverlibrary";
-import { TypeScriptServiceHost } from "./typescript_host";
-
-/**
- * Create an instance of a Deno `LanguageService`.
- */
-export function createLanguageService(host: TypeScriptServiceHost) {
-  console.log(host);
-}
