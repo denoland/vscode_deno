e4149d49fea825b05c7ff60a333ec57377537a68
Jason <m.jason.liu@outlook.com>
2020-09-18 19:30:33 +0800

    WIP

---
+++ client/src/debug_config_provider.ts
@@ -0,0 +1,74 @@
+import {
+  ExtensionContext,
+  debug,
+  WorkspaceFolder,
+  DebugConfiguration,
+  DebugConfigurationProvider,
+  ProviderResult,
+  window,
+} from "vscode";
+
+export interface DenoCmd {
+  name: string;
+  entrypoint: string;
+  security_flags: string;
+  port: string;
+}
+
+export function activeDenoDebug(context: ExtensionContext) {
+  context.subscriptions.push(
+    debug.registerDebugConfigurationProvider(
+      "deno",
+      new DenoDebugConfigurationProvider()
+    )
+  );
+}
+
+export class DenoDebugConfigurationProvider
+  implements DebugConfigurationProvider {
+  provideDebugConfigurations(
+    folder: WorkspaceFolder | undefined
+  ): ProviderResult<DebugConfiguration[]> {
+    console.log(folder);
+    return null;
+  }
+
+  resolveDebugConfiguration(
+    workspace: WorkspaceFolder | undefined,
+    config: DebugConfiguration
+  ): ProviderResult<DebugConfiguration> {
+    // if launch.json is missing or empty
+    if (!config.type && !config.request && !config.name) {
+      const editor = window.activeTextEditor;
+      const langid = editor?.document.languageId;
+      if (
+        editor &&
+        (langid === "typescript" ||
+          langid === "javascript" ||
+          langid === "typescriptreact" ||
+          langid === "javascriptreact")
+      ) {
+        config.__workspaceFolder = workspace?.uri.fsPath;
+        config.type = "pwa-node";
+        config.name = "Launch";
+        config.request = "launch";
+        config.program = "${file}";
+        config.runtimeExecutable = "deno";
+        config.runtimeArgs = ["run", "--inspect-brk", "-A"];
+        config.attachSimplePort = 9229;
+        return config;
+      }
+      return null;
+    }
+
+    if (!config.program) {
+      return window
+        .showInformationMessage("Cannot find a program to debug")
+        .then((_) => {
+          return undefined; // abort launch
+        });
+    }
+
+    return config;
+  }
+}
