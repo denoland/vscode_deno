fd404efd1a23c7ee9b81795743d0f7150725c719
CGQAQ <m.jason.liu@outlook.com>
2020-09-21 00:17:37 +0800

    don't give any permission by default
    Co-authored-by: Luca Casonato <luca.casonato@antipy.com>

--- client/src/debug_config_provider.ts
+++ client/src/debug_config_provider.ts
@@ -64,7 +64,7 @@ export class DenoDebugConfigurationProvider
           type: "pwa-node",
           program: "${file}",
           runtimeExecutable: "deno",
-          runtimeArgs: ["run", "--inspect-brk", "-A"],
+          runtimeArgs: ["run", "--inspect-brk"],
           attachSimplePort: 9229,
         });
         return undefined;
