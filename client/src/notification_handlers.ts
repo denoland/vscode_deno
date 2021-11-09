4e1036c6fb74e0a79d61111b41d993e5aa501be4
Kitson Kelly <me@kitsonkelly.com>
2021-11-09 21:54:10 +1100

    fix: do not cache unsupported origins in config (#571)
    Ref denoland/deno#12621

--- client/src/notification_handlers.ts
+++ client/src/notification_handlers.ts
@@ -8,25 +8,25 @@ export function createRegistryStateHandler(): NotificationHandler<
   RegistryStateParams
 > {
   return async function handler({ origin, suggestions }) {
-    let enable = false;
     if (suggestions) {
       const selection = await vscode.window.showInformationMessage(
         `The server "${origin}" supports completion suggestions for imports. Do you wish to enable this? (Only do this if you trust "${origin}") [Learn More](https://github.com/denoland/vscode_deno/blob/main/docs/ImportCompletions.md)`,
         "No",
         "Enable",
       );
-      enable = selection === "Enable";
+      const enable = selection === "Enable";
+      const suggestImportsConfig = vscode.workspace.getConfiguration(
+        "deno.suggest.imports",
+      );
+      const hosts: Record<string, boolean> =
+        suggestImportsConfig.get("hosts") ??
+          {};
+      hosts[origin] = enable;
+      await suggestImportsConfig.update(
+        "hosts",
+        hosts,
+        vscode.ConfigurationTarget.Workspace,
+      );
     }
-    const suggestImportsConfig = vscode.workspace.getConfiguration(
-      "deno.suggest.imports",
-    );
-    const hosts: Record<string, boolean> = suggestImportsConfig.get("hosts") ??
-      {};
-    hosts[origin] = enable;
-    await suggestImportsConfig.update(
-      "hosts",
-      hosts,
-      vscode.ConfigurationTarget.Workspace,
-    );
   };
 }
