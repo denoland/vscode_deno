baabfc2bf9c77801a034d1a8e49d42bb6121aa81
Nayeem Rahman <nayeemrmn99@gmail.com>
2024-10-21 22:09:09 +0100

    feat: "deno.trace.server" setting

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -7,7 +7,7 @@ export const ENABLEMENT_FLAG = "deno:lspReady";
 export const EXTENSION_ID = "denoland.vscode-deno";
 export const EXTENSION_NS = "deno";
 export const EXTENSION_TS_PLUGIN = "typescript-deno-plugin";
-export const LANGUAGE_CLIENT_ID = "deno-language-server";
+export const LANGUAGE_CLIENT_ID = "deno";
 export const LANGUAGE_CLIENT_NAME = "Deno Language Server";
 export const TS_LANGUAGE_FEATURES_EXTENSION =
   "vscode.typescript-language-features";
