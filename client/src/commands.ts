485906c3e604e12bbed33c3fa2a8acba1345eea7
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-02-26 14:46:01 +0000

    fix

--- client/src/commands.ts
+++ client/src/commands.ts
@@ -40,7 +40,6 @@ import * as path from "path";
 import * as process from "process";
 import * as jsoncParser from "jsonc-parser/lib/esm/main.js";
 import { semver } from "./semver";
-import { log } from "./extension";
 
 // deno-lint-ignore no-explicit-any
 export type Callback = (...args: any[]) => unknown;
